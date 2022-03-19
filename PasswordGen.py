9ff4a2bb09b107eeb44e65df9af9f9a67940fad8
nathanielam0ah <nkp.amoah@gmail.com>
2022-03-19 11:14:31 +0000

    rm junk variables.

--- PasswordGen.py
+++ PasswordGen.py
@@ -1,28 +1,24 @@
-
+#!/usr/bin/env python3
 import random, string
 
-theLetters = string.ascii_letters
-capitalLts = theLetters.upper()
-spcCharacters = string.punctuation
-dgts = string.digits
-def GetLength(): # Gets the desired password length
-    print("Type the Desired Length")
-    length = input(": ")
-    if length == "exit": exit()
-    return int(length) if length.isdigit() else 0
-def Generator(length=8): # Funtion to generate password based on random
-    printable = f'{theLetters}{capitalLts}{dgts}{spcCharacters}'
-    printable = list(printable)
-    random.shuffle(printable)
-    random_password = random.choices(printable, k=length)
-    random_password = ''.join(random_password)
-    return random_password
+def GetLength(): #Receives desired length
+	length=input("Type the desired length (x to exit): ")
+	if length=="x": exit()
+	return int(length) if length.isdigit() else 0
+
+def Generator(length=8): #Generates random ascii string
+	printable=f"{string.ascii_letters}{string.ascii_letters.upper()}{string.digits}{string.punctuation}"
+	printable=list(printable)
+	random.shuffle(printable)
+	initPassword=random.choices(printable,k=length)
+	initPassword="".join(initPassword)
+	return initPassword
 
 while 1:
-    passwd_length = GetLength()
-    if passwd_length == 0:
-        passwd = Generator()
+    passwd_length=GetLength()
+    if passwd_length==0:
+        passwd=Generator()
         print(f'password (default=8):\t\t{passwd}')
     else:
-        passwd = Generator(passwd_length)
+        passwd=Generator(passwd_length)
         print(f'password ({str(len(passwd))}):\t\t{passwd}')
